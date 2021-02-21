719bfe42bc72ed811d878fe73dae40ad7e9822b6
Dalia <dalia.metwaly88@gmail.com>
2021-02-21 13:25:33 +0200

    fix socket url

--- public/js/sync.js
+++ public/js/sync.js
@@ -20,7 +20,7 @@ function setup(guidParam,tokenParam,entityIdParam){
 // const socket = io('http://192.168.1.100:3000');
   // socket = io('http://localhost:3000');
   socket = io('https://vp-sync-staging.herokuapp.com/');
- alert('socketsync'+socket)
+
 socket.on('disconnect', () => {
   mainStatus.innerHTML = `<span style="color:red">Disconnected</span>`;
 });
